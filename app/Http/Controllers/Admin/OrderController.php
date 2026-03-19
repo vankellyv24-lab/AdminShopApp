b1fcbe60a2cd23c0b8c29300a12ad6f107ea252d
VanKelly <vankellyv2.0@gmail.com>
2026-03-19 20:31:36 +0800

    Fix missing ActivityLog import in OrderController

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\ActivityLog;
 use App\Models\Order;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
